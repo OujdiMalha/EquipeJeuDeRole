ef9c09e5328684c5028f61fca0f02ce9a306dd06
OujdiMalha <mohammed.malha@ifosup.wavre.be>
2023-12-16 12:24:27 +0100

    Menu 1.7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using EquipeJeuDeRole;
+using EquipeJeuDeRole.Pages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +9,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<EntityService>();
 await builder.Build().RunAsync();
